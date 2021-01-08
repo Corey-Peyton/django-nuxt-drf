3053587f81e60fce4c102df583a40cfb5cf744ad
stevensam0109 <steven.sam.0109@outlook.com>
2021-01-08 18:29:13 -0500

    added STATIC_ROOT

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "assets", "static")
 
 
 REST_FRAMEWORK = {
